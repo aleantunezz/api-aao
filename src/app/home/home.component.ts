932e61c63e4dfb32d319c53bd50c95a20d682e82
aleantunezz <aleeantunezortiz@gmail.com>
2023-12-11 11:04:55 +0100

    modified:   angular.json 	modified:   src/app/app-routing.module.ts 	modified:   src/app/app.component.html 	modified:   src/app/app.module.ts 	new file:   src/app/home/home.component.html 	new file:   src/app/home/home.component.scss 	new file:   src/app/home/home.component.spec.ts 	new file:   src/app/home/home.component.ts 	new file:   src/app/service/api.service.spec.ts 	new file:   src/app/service/api.service.ts

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  data: CharacterData[] = [];
+
+  constructor(private apiService: ApiService) { }
+
+  ngOnInit(): void {
+    this.llenarData();
+  }
+
+  llenarData() {
+    this.apiService.getData().subscribe(data => {
+      this.data = data.results; // Asegúrate de acceder a la propiedad correcta
+      console.log(this.data);
+    });
+  }
+}
+
+// Define esta interfaz según la estructura de tu API
+interface CharacterData {
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: {
+    name: string;
+    url: string;
+  };
+  location: {
+    name: string;
+    url: string;
+  };
+  image: string;
+  episode: string[];
+  url: string;
+  created: string;
+}
+
+
